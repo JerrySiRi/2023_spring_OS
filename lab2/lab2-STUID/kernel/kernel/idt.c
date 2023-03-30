8f18a285c0445ceac47d1dd8026ca46c254f653d
JerrySiRi <15210723175@163.com>
2023-03-31 02:01:30 +0800

    initialize idt

--- lab2/lab2-STUID/kernel/kernel/idt.c
+++ lab2/lab2-STUID/kernel/kernel/idt.c
@@ -10,11 +10,30 @@ struct GateDescriptor idt[NR_IRQ]; // NR_IRQ=256, defined in x86/cpu.h
 /* 初始化一个中断门(interrupt gate) */
 static void setIntr(struct GateDescriptor *ptr, uint32_t selector, uint32_t offset, uint32_t dpl) {
 	// TODO: 初始化interrupt gate
+	// ptr是指向这个256个门描述符中的某一个元素，使用传进来的三个参数来初始化它。除此之外其他信息是固定的
+	ptr->offset_15_0 = offset & 0xFFFF;//类型是unit32_t，右移动高位补0.----------------自己写的：(offset<<16)>>16;
+	ptr->offset_31_16 = (offset>>16)&0xFFFF;//通过与上一个长度更小的【显示地把】长度给截断-------自己写的，不加0xfffff
+	ptr->segment = selector<<3;//自动截断，回顾lab1中的gdt表，选择子存入寄存器后，低三位是留给特权级的！！！
+	ptr->present = 0x1;
+	ptr->privilege_level = dpl;
+	ptr->system = 0;
+	ptr->type = 0xE;//也就是上面定义的INTERRUPT_GATE_32
+	ptr->pad0 = 0;
+
+
 }
 
 /* 初始化一个陷阱门(trap gate) */
 static void setTrap(struct GateDescriptor *ptr, uint32_t selector, uint32_t offset, uint32_t dpl) {
 	// TODO: 初始化trap gate
+	ptr->offset_15_0 = offset & 0xFFFF;//类型是unit32_t，右移动高位补0.----------------自己写的：(offset<<16)>>16;
+        ptr->offset_31_16 = (offset>>16)&0xFFFF;//通过与上一个长度更小的【显示地把】长度给截断-------自己写的，不加0xfffff
+        ptr->segment = selector<<3;//自动截断，回顾lab1中的gdt表，选择子存入寄存器后，低三位是留给特权级的！！！
+        ptr->present = 0x1;
+        ptr->privilege_level = dpl;
+        ptr->system = 0;
+        ptr->type = 0xF;//也就是上面定义的TRAP_GATE_32
+        ptr->pad0 = 0;
 }
 
 /* 声明函数，这些函数在汇编代码里定义 */
@@ -43,13 +62,24 @@ void initIdt() {
 	 * init your idt here
 	 * 初始化 IDT 表, 为中断设置中断处理函数
 	 */
-	/* Exceptions with error code */
+	/* Exceptions with error code 【针对某些特定的中断向量，压入error code】*/
+	//static void setTrap(struct GateDescriptor *ptr, uint32_t selector, uint32_t offset, uint32_t dpl)函数声明
+
 	setTrap(idt + 0x8, SEG_KCODE, (uint32_t)irqDoubleFault, DPL_KERN);
 	// TODO: 填好剩下的表项
+       	setTrap(idt + 0x8, SEG_KCODE, (uint32_t)irqDoubleFault, DPL_KERN);
+	 setTrap(idt + 0xa, SEG_KCODE, (uint32_t)irqInvalidTSS, DPL_KERN);
+	 setTrap(idt + 0xb, SEG_KCODE, (uint32_t)irqSegNotPresent, DPL_KERN);
+	 setTrap(idt + 0xc, SEG_KCODE, (uint32_t)irqStackSegFault, DPL_KERN);
+	 setTrap(idt + 0xd, SEG_KCODE, (uint32_t)irqGProtectFault, DPL_KERN);
+	 setTrap(idt + 0xe, SEG_KCODE, (uint32_t)irqPageFault, DPL_KERN);
+	 setTrap(idt + 0x11, SEG_KCODE, (uint32_t)irqAlignCheck, DPL_KERN);
+	 setTrap(idt + 0x1e, SEG_KCODE, (uint32_t)irqSecException, DPL_KERN);
 	
 	/* Exceptions with DPL = 3 */
 	// TODO: 填好剩下的表项 
-
+	 setTrap(idt + 0x21, SEG_KCODE, (uint32_t)irqKeyboard, DPL_KERN);
+	 setIntr(idt + 0x80, SEG_KCODE, (uint32_t)irqSyscall, DPL_USER); // for int 0x80, interrupt vector is 0x80, Interruption is disabled
 	/* 写入IDT */
 	saveIdt(idt, sizeof(idt));
 }
